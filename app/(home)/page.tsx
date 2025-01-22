d04d65dfae88476eb1a0c4f47f7c18a076dcd427
Hilton <htn.contato@hotmail.com>
2025-01-22 20:42:12 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,20 @@
+import { format } from "date-fns"
 import Header from "@/components/header"
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="p-5"> 
+        <h2 className="text-xl font-bold">Olá, usuario</h2>
+        <p className="capitalize text-sm">
+          { format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR
+          })}
+        </p>
+      </div>
     </div>
   );
 }
