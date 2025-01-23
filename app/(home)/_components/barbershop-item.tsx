291a4e50c3431db136048112c24c2542a3568e20
Hilton <htn.contato@hotmail.com>
2025-01-23 17:02:58 -0300

    feat: add BarbershopInfo component

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -1,15 +1,25 @@
+"use client";
+
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Barbershop } from "@prisma/client";
 import { StarIcon } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface BarbershopItemProps {
   barbershop: Barbershop
 }
 
 const BarberShopItem = ({ barbershop }: BarbershopItemProps) => {
+  
+  const router = useRouter();
+
+  const handleBookingClick = () => {
+    router.push(`/barbershops/${barbershop.id}`);
+  }
+
   return ( 
     <Card className="min-w-[165px] max-w-[165px] rounded-2xl">
       <CardContent className="py-0 px-1">
@@ -32,7 +42,7 @@ const BarberShopItem = ({ barbershop }: BarbershopItemProps) => {
         <div className="px-2 pb-3">
           <h2 className="font-bold text-lg mt-2  overflow-hidden text-ellipsis text-nowrap">{barbershop.name}</h2>
           <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">{barbershop.address}</p>
-          <Button className="w-full mt-3" variant="secondary">Reservar</Button>
+          <Button className="w-full mt-3" variant="secondary" onClick={handleBookingClick}>Reservar</Button>
         </div>
       </CardContent>
     </Card>
