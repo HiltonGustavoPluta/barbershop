2544ec44f3254e6e94ec9fa29043cd4760506f1a
Hilton <htn.contato@hotmail.com>
2025-01-29 20:35:04 -0300

    fix: test

--- app/_lib/prisma.ts
+++ app/_lib/prisma.ts
@@ -1,10 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-// Evita múltiplas instâncias do Prisma no ambiente de desenvolvimento
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-
-export const db = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = db;
+declare global {
+  var cachedPrisma: PrismaClient;
 }
+
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prisma = global.cachedPrisma;
+}
+
+export const db = prisma;
